b9af1b2486f8c787f522358731b8af1fdbe8f774
nnaelmuna <naelmuna2009@gmail.com>
2025-10-23 10:48:22 +0700

    finishing routing app_page

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,28 @@
+import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:news_app/screens/home_screen.dart';
+import 'package:news_app/screens/news_detail_screen.dart';
+import 'package:news_app/screens/splash_screen.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.SPLASH;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => SplashScreen(),
+    ),
+    GetPage(
+      name: _Paths.HOME,
+      page: () => HomeScreen(),
+      bindings: HomeBindings(),
+    ),
+    GetPage(
+      name: _Paths.NEWS_DETAIL,
+      page: () => NewsDetailScreen(),
+    ),
+  ];
+}
